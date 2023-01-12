adca16a05a664090c43557068ebf1ae8dc61d676
Roger Kristiansen <roger.kristiansen@norgesgruppen.no>
2023-01-12 14:06:09 +0100

    Cleanup Linting

--- docker/snorkelground_voice_chatgpt.py
+++ docker/snorkelground_voice_chatgpt.py
@@ -36,7 +36,7 @@ def get_audio_input():
     Prints "Mikrofonen er aktiv... (si ordet 'avbryt' for å avslutte programmet)"
     while it is listening, and "Mikrofonen er slått av igjen..." when it is finished.
     Returns the recorded audio."""
-    with sr.Microphone() as source:    
+    with sr.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source, duration=1)
         print("Mikrofonen er aktiv... (si ordet 'avbryt' for å avslutte programmet)")
         audio = recognizer.listen(source)
